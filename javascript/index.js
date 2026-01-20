83f34f7a9e38feed3a3d7766d6ab12a71ef614a0
Christopher G <tormentionrex@gmail.com>
2026-01-20 16:37:33 -0600

    guardar

---
+++ javascript/index.js
@@ -0,0 +1,47 @@
+let  primerDigito = 10
+
+let segundoDigito = 5
+
+let suma = 10 + 5
+
+console.log(suma);
+
+
+
+let palabra = "Parangaricutirimicuaro"
+
+console.log(palabra.length);
+
+
+const varPi = 3.14
+
+const varRadio = 7
+
+let area = varPi * (varRadio * varRadio)
+
+console.log(area);
+
+
+
+
+
+
+
+
+
+let primerNombre = "Christopher"
+
+let segundoNombre = "Gerardo"
+
+let NombreCompleto = primerNombre.concat(segundoNombre);
+
+console.log(NombreCompleto);
+
+
+let Paises = ["USA", "PERU", "PANAMA", "CHILE"]
+
+let Incluir = Paises.includes("Venezuela")
+
+console.log(Incluir);
+
+
---
+++ javascript/practica2.js
@@ -0,0 +1,26 @@
+const numeros = [5, 10, 15, 20]
+
+console.log( "los numeros son", numeros );
+
+let suma = [ "El resultado de la suma es igual a" ,  5 + 10 + 15 + 20]
+
+console.log(suma);
+
+
+
+const persona = {
+  nombre: "Carlos",
+  edad: 25,
+  ciudad: "San José"
+};
+
+console.log( "el nombre de la persona es:", persona.nombre);
+
+console.log( "la edad de la persona es:",  persona.edad);
+
+//cambio 
+persona.ciudad = "Heredia";
+
+console.log( "la ciudad de la persona es:", persona.ciudad);
+
+
